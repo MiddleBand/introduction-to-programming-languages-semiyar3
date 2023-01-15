bfb731a59f636d0a830929ccd3c8731917f77180
Vladislav <milliganiam6@gmail.com>
2023-01-15 21:12:19 +0600

    добавлен task27

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetMultiply(num)}");
+
+
+int GetMultiply(int number)
+{
+int result = 0;
+for (int i = 1; i <= number; i++)
+{
+result += i;
+}
+return result;
+}
