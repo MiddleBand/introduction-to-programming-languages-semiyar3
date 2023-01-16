a10a40c48f821193adcb88b4be657c7c27ef13fa
Vladislav <milliganiam6@gmail.com>
2023-01-16 12:55:10 +0600

    добавлен task292

--- task29/Program.cs
+++ task29/Program.cs
@@ -3,20 +3,20 @@
 // 6, 1, 33 -> [6, 1, 33]
 
 
-// int[] array = GetBinaryArray(8);
-// Console.WriteLine($" [ {String.Join(" | ", array)} ] ");
+int[] array = GetBinaryArray(8);
+Console.WriteLine($" [ {String.Join(" | ", array)} ] ");
 
 
-// int[] GetBinaryArray(int size)
-// {
-// int[] result = new int[size];
-// for (int i = 0; i < size; i++)
-// {
-// result[i] = new Random().Next(16);
-// }
+int[] GetBinaryArray(int size)
+{
+int[] result = new int[size];
+for (int i = 0; i < size; i++)
+{
+result[i] = new Random().Next(16);
+}
 
-// return result;
-// }
+return result;
+}
 
 
 
