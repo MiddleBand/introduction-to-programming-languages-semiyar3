5d30cd5a47827f345a41e69d8598f7936ae37b8f
Vladislav <milliganiam6@gmail.com>
2023-01-17 22:36:29 +0600

    добавлен task3636

--- task36/Program.cs
+++ task36/Program.cs
@@ -4,13 +4,33 @@
 // [-4, -6, 89, 6] -> 0
 
 
-int[] numbers = {-4, -6, 89, 6};
-int lengthOfNumbers = numbers.Length;
-int i = 0;
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
 int sum = 0;
 
-for (i = 0; i < lengthOfNumbers; i++)
- if(i % 2 == 1) 
- sum = sum + numbers[i];
+for (int z = 0; z < numbers.Length; z+=2)
+    sum = sum + numbers[z];
 
-Console.WriteLine(sum);
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(1,10);
+        }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
