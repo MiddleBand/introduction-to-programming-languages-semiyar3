9687a502d245c245155b4f4b0780b2ab35390ac8
no name <120008252+MiddleBand@users.noreply.github.com>
2023-01-28 22:07:01 +0300

    Two-dimensional_arrays

--- abstract/Program.cs
+++ abstract/Program.cs
@@ -309,42 +309,7 @@ return array;
 //Задайте массив. Напишите программу, которая определяет,
 // присутствует ли заданное число в массиве.
 
-Clear();
-Write("Введите элементы массива через пробел: ");
-string elements = ReadLine();
-int[] baseArray = GetArrayFromString(elements);
-Write("Введите число: ");
-int n = int.Parse(ReadLine());
-if (FindElement(baseArray, n))
-{
-WriteLine("Да");
-}
-else
-{
-WriteLine("Нет");
-}
-
-
-int[] GetArrayFromString(string stringArray)
-{
-string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-int[] res = new int[nums.Length];
-
-for (int i = 0; i < nums.Length; i++)
-{
-res[i] = int.Parse(nums[i]);
-}
-return res;
-}
-
-bool FindElement(int[] array, int el)
-{
-foreach (var item in array)
-{
-if (el == item) return true;
-}
-return false;
-}
+00000000
 
 
 
@@ -885,3 +850,264 @@ for (int i = 1; i < 50; i++)
 
 
 
+// /Задайте двумерный массив размером m×n,
+//заполненный случайными целыми числами
+
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+int[,] result = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+for (int j = 0; j < n; j++)
+{
+result[i, j] = new Random().Next(minValue, maxValue + 1);
+}
+}
+return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+for (int i = 0; i < inArray.GetLength(0); i++)
+{
+for (int j = 0; j < inArray.GetLength(1); j++)
+{
+Console.Write($"{inArray[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+
+
+
+// Матрица случайных чисел
+Console.Clear();
+
+int [,] matrix = new int[4,4];
+FillArray(matrix);
+Console.WriteLine("Задача 48.");
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Задача 49.");
+FillArraySqr(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Задача 51.");
+FillArrayDiag(matrix);
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write($"{matr[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+matr[i,j] = i + j;
+}
+}
+}
+
+void FillArraySqr(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+if (i % 2 == 0 && j % 2 == 0) matr[i,j] = matr[i,j] * matr[i,j];
+}
+}
+}
+
+void FillArrayDiag(int
+
+
+
+
+// 0/ Матрица случайных чисел
+Console.Clear();
+
+int [,] matrix = new int[4,4];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+FillArrayMidl(matrix);
+
+
+void PrintArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+Console.Write($"{matr[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+for (int j = 0; j < matr.GetLength(1); j++)
+{
+matr[i,j] = i + j;
+}
+}
+}
+
+void FillArrayMidl(int[,] matr)
+{
+Console.Write("Среднее: ");
+for (int i = 0; i < matr.GetLength(1); i++)
+{
+double sum = 0;
+for (int j = 0; j < matr.GetLength(0); j++)
+{
+sum += matr[i,j];
+}
+Console.Write($"{sum/matr.GetLength(0)}, ");
+}
+}
+                   
+                   
+                   
+ 
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.Write("Введите значение элемента: ");
+int element=int.Parse(Console.ReadLine());
+
+if(FindeElemrnt(array, element))
+{
+    Console.WriteLine("Найден");
+}
+else
+{
+    Console.WriteLine("Не найден");
+}
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+ int[,] result = new int[m, n];
+ for (int i = 0; i < m; i++)
+ {
+  for (int j = 0; j < n; j++)
+   {
+   result[i, j] = new Random().Next(minValue, maxValue + 1);
+   }
+ }
+ return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+for (int i = 0; i < inArray.GetLength(0); i++)
+{
+for (int j = 0; j < inArray.GetLength(1); j++)
+{
+Console.Write($"{inArray[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+bool FindeElemrnt(int[,] array, int el)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i, j] == el) return true;
+        }
+    }
+    return false;
+}
+                   
+                   
+                
+ 
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+                   
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+ int[,] result = new int[m, n];
+ for (int i = 0; i < m; i++)
+ {
+  for (int j = 0; j < n; j++)
+   {
+   result[i, j] = new Random().Next(minValue, maxValue + 1);
+   }
+ }
+ return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+for (int i = 0; i < inArray.GetLength(0); i++)
+{
+for (int j = 0; j < inArray.GetLength(1); j++)
+{
+Console.Write($"{inArray[i,j]} ");
+}
+Console.WriteLine();
+}
+}                   
