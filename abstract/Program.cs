a80ee186c5b9b0ef8deb92ddbd3da9f884230c9b
no name <120008252+MiddleBand@users.noreply.github.com>
2023-01-29 11:01:56 +0300

    чва

--- abstract/Program.cs
+++ abstract/Program.cs
@@ -1111,3 +1111,112 @@ Console.Write($"{inArray[i,j]} ");
 Console.WriteLine();
 }
 }                   
+
+                   
+                   
+                   
+//                   
+void PrintArray(int[,] matr) // Создание матрицы
+{
+    for (int i = 0; i  < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr) // Заполнение массива рандомными данными
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void ChangeRow(int [,] array){
+
+  int temp = 0;
+
+  for(int i=0; i < array.GetLength(0); i++){
+    temp = array[0, i];
+    array[0, i] = array[array.GetLength(0)-1, i];
+    array[array.GetLength(0)-1, i] = temp;
+  }
+
+}
+
+
+int [, ] array = new int [5, 5];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine();
+
+ChangeRow(array);
+PrintArray(array);
+                   
+                   
+                   
+//                   
+void PrintArray(int[,] matr) // Создание матрицы
+{
+    for (int i = 0; i  < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr) // Заполнение массива рандомными данными
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+void ChangeRow(int [,] array){
+
+  int temp = 0;
+
+  for(int i=0; i < array.GetLength(0); i++){
+    temp = array[0, i];
+    array[0, i] = array[array.GetLength(0)-1, i];
+    array[array.GetLength(0)-1, i] = temp;
+  }
+
+}
+
+int [,] Transposition (int [,] array){
+
+  int [,] newArray = new int [array.GetLength(1), array.GetLength(0)];
+
+  // Производим транспонирование
+  for(int i=0; i<newArray.GetLength(0); i++)
+    for(int j=0; j<newArray.GetLength(1); j++)
+      newArray[i,j] = array[j,i];
+
+  return newArray;
+}
+
+int [,] array = new int [7, 5];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine();
+
+int [, ] newArray = Transposition(array);
+PrintArray(newArray);
